8da5fa745b690712c31f33cc8ff02f7cd6c55b82
K N Monish Kumar <monishkumar810@gmail.com>
2023-04-26 12:11:40 +0530

    waste

--- client/src/helpers/axiosInstance.js
+++ client/src/helpers/axiosInstance.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 export const axiosInstance = axios.create({
   headers: {
     Authorization: `Bearer ${localStorage.getItem("token")}`,
